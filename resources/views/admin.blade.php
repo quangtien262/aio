27f1d9fe9a4604b765da52d9e9453e68ac426dd0
TienLQ <quangtienvkt@gmail.com>
2026-06-01 15:51:27 +0700

    ok

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -5,7 +5,9 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>{{ config('app.name', 'AIO Platform') }} Admin</title>
-        @viteReactRefresh
+        @env('local')
+            @viteReactRefresh
+        @endenv
         @vite('resources/admin/src/main.jsx')
     </head>
     <body>
